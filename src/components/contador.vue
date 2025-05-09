ba3e964564ba2157203a9ff36ed9e480393ad9cb
Jose Bolivar <josebolivarbolivar97@gmail.com>
2025-05-09 16:01:09 -0500

    segundo commint

---
+++ src/components/contador.vue
@@ -0,0 +1,26 @@
+<template>
+<h1>hola mundo</h1>
+<p>{{ contador }}</p>
+<button @click="incrementar">incrementar</button>
+<button @click="decrementar">Decrementar</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const contador=ref(0);
+
+
+
+const incrementar=() =>{
+  contador.value++;
+}
+
+const decrementar=() =>{
+  contador.value--;
+}
+</script>
+
+<style>
+
+</style>
